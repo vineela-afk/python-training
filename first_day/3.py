2abedcc39adbb69ac766856bc1c540b0939f49a2
vineela-afk <ampilivineela@gmail.com>
2023-07-03 18:27:48 +0530

    final

--- first_day/3.py
+++ first_day/3.py
@@ -3,8 +3,8 @@ a, b, c = input("Enter the values: ").split()
     a ,b ,c are three numbers and we are checking the larger of the three
 '''
 if a > b and a > c:
-    print("Larger number is: " + a)
+    print(a)
 elif b > a and b > c:
-    print("Larger number is: " + b)
+    print(b)
 else:
-    print("Larger number is: " + c)
+    print(c)

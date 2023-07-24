f82fea6a9c546f70bd0c9e12f40cf151d9941c5d
vineela-afk <62927278+vineela-afk@users.noreply.github.com>
2023-07-24 15:25:59 +0530

    Update 2.py
    done

--- sixth_day/2.py
+++ sixth_day/2.py
@@ -10,7 +10,7 @@ def check_phishing_url(url):
     params = {
         "url": url,
         "format": "json",
-        "app_key": "<YOUR_PHISHTANK_APP_KEY>"
+        "app_key": "<APP_KEY>"
     }
     retries = Retry(total=3, backoff_factor=0.5, status_forcelist=[500, 502, 503, 504])
     session = requests.Session()
@@ -26,15 +26,5 @@ def check_phishing_url(url):
         return f"{url} is not a phishing URL."
     except requests.exceptions.RequestException as e:
         return f"An error occurred: {str(e)}"
-
-
-def demo():
-    url = input("Enter a URL to check if it is a phishing URL: ")
-    result = check_phishing_url(url)
-    print(result)
-
-
-# Run the demo function
-demo()
 
 
